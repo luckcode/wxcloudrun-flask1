9a1d4907a9f5234e774c833a782f8b63ba38fbbb
luckcode <shellexecuteExA@gmail.com>
2022-07-18 10:36:24 +0800

    delete re

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -69,7 +69,7 @@ def get_count():
 def ufile():
     try:
         file = request.files['file']
-        #r = re.search(r'(\.\S+)', file.filename)
+        #r = re.search(r'(\.\S+)', file.filename) 
         file.save(u_path + './static/' + str(time.time())+file.filename) # 没有判断文件夹是否存在，需自己先创建一个
         return make_err_response('success')
     except Exception as e:
